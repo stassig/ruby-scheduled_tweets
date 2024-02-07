f09ce67a392b5825e38705fc63740718cd20334b
STANISLAV PETKOV <stassig164@gmail.com>
2024-02-07 16:39:50 +0100

    basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # GET /about
   get "about", to: "about#index"
+
+  get "password", to: "passwords#edit"
+  patch "password", to: "passwords#update"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
 end
