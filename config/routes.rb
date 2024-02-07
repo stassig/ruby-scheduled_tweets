c07f941d5c5aabcaf2888cdcf4038688ecb9ae72
STANISLAV PETKOV <stassig164@gmail.com>
2024-02-07 17:25:04 +0100

    authentication update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
-  get "password", to: "passwords#edit"
+  get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
 
   get "sign_up", to: "registrations#new"
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
 
   root to: "main#index"
 end
