0fb21dd520becec9d9c87a145fdc61be03e737b2
STANISLAV PETKOV <stassig164@gmail.com>
2024-02-08 10:18:59 +0100

    Password Reset

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,13 +1,8 @@
 class PasswordMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.password_mailer.reset.subject
-  #
   def reset
-    @greeting = "Hi"
+    @token = params[:user].signed_id(purpose: "password_reset", expires_in: 15.minutes)
 
-    mail to: "to@example.org"
+    mail to: params[:user].email
   end
 end
